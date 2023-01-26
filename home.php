2fb6519fd2926256eb4389216e8da4039a723109
Satyajit Panchal <satyajitpanchal2000@gmail.com>
2023-01-26 20:24:04 +0530

    home content
    not css

---
+++ home.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+
+	<title>head</title>
+
+	 <!-- font cdn link  -->
+   <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0/css/all.min.css">
+</head>
+<body>
+	<section class="home">
+
+   <div class="content">
+      <h3>Hand Picked Book to your door.</h3>
+      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Excepturi, quod? Reiciendis ut porro iste totam.</p>
+      <a href="about.php" class="white-btn">discover more</a>
+   </div>
+
+</section>
+
+</body>
+</html>
